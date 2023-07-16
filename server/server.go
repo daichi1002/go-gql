27c49ae957440f54db076eb451d3410208a28b90
daichinagano <d.nagano1002@gmail.com>
2023-07-16 10:55:57 +0900

    :art: add cors

--- server/server.go
+++ server/server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/daichi1002/go-graphql/adapters/graph/generated"
 	"github.com/daichi1002/go-graphql/adapters/graph/resolvers"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
@@ -20,9 +21,10 @@ func Serve() {
 	}
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &resolvers.Resolver{}}))
-
+	// CORS対応
+	handler := cors.Default().Handler(srv)
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/query", handler)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
